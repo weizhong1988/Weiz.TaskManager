5d09dc0c61840f4a1e740611af4891e79cf0e05d
weizhong1988 <weizhong1988@gmail.com>
2016-11-11 13:58:07 +0800

    no message

---
+++ Weiz.TaskManager.Common/Util/PerformanceTracer.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
+
+namespace Weiz.TaskManager.Common
+{
+    /// <summary>
+    /// 性能跟踪工具。
+    /// </summary>
+    public static class PerformanceTracer
+    {
+        private static int performanceTracer;
+
+        static PerformanceTracer()
+        {
+            if (!string.IsNullOrWhiteSpace(ConfigurationManager.AppSettings["PerformanceTracer"]))
+            {
+                int.TryParse(ConfigurationManager.AppSettings["PerformanceTracer"], out performanceTracer);
+            }
+            else
+            {
+                performanceTracer = -1;
+            }
+        }
+
+        /// <summary>
+        /// 跟踪指定代码块的执行时间。
+        /// </summary>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="func"></param>
+        /// <param name="traceName">跟踪名称。</param>
+        /// <returns>返回类型为<typeparamref name="TResult"/>的结果。</returns>
+        public static TResult Invoke<TResult>(Func<TResult> func, string traceName = null)
+        {
+            if (performanceTracer >= 0)
+            {
+                if (string.IsNullOrWhiteSpace(traceName))
+                {
+                    var method = new StackTrace().GetFrames()[1].GetMethod();
+
+                    traceName = string.Format("{0}.{1}", method.ReflectedType.FullName, method.Name);
+                }
+
+                Stopwatch stopwatch = new Stopwatch();
+
+                stopwatch.Start();
+
+                TResult result = func();
+
+                stopwatch.Stop();
+
+                if (stopwatch.Elapsed.TotalMilliseconds > performanceTracer)
+                {
+                    if (!Debugger.IsAttached)
+                    {
+                        LogHelper.WriteLog("性能问题(" + traceName + ")" + stopwatch.Elapsed.TotalMilliseconds + "ms");
+                    }
+
+                    Debug.WriteLine("性能问题({0})，耗时{1}毫秒。", traceName, stopwatch.Elapsed.TotalMilliseconds);
+                }
+
+                return result;
+            }
+            else
+            {
+                return func();
+            }
+        }
+
+        /// <summary>
+        /// 调用并且跟踪指定代码块的执行时间。
+        /// </summary>
+        /// <param name="action"></param>
+        /// <param name="traceName">跟踪名称。</param>
+        /// <param name="enableThrow">指定是否允许抛出异常。</param>
+        public static void Invoke(Action action, string traceName = null, bool enableThrow = true)
+        {
+            if (performanceTracer >= 0)
+            {
+                if (string.IsNullOrWhiteSpace(traceName))
+                {
+                    var method = new StackTrace().GetFrames()[1].GetMethod();
+
+                    traceName = string.Format("{0}.{1}", method.ReflectedType.FullName, method.Name);
+                }
+
+                try
+                {
+                    Stopwatch stopwatch = new Stopwatch();
+
+                    stopwatch.Start();
+
+                    action();
+
+                    stopwatch.Stop();
+
+                    if (stopwatch.Elapsed.TotalMilliseconds > performanceTracer)
+                    {
+                        if (!Debugger.IsAttached)
+                        {
+                            LogHelper.WriteLog("性能问题(" + traceName + ")" + stopwatch.Elapsed.TotalMilliseconds + "ms");
+                        }
+
+                        Debug.WriteLine("性能问题({0})，耗时{1}毫秒。", traceName, stopwatch.Elapsed.TotalMilliseconds);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    if (enableThrow)
+                    {
+                        throw;
+                    }
+
+                    //LogHelper.HandleError(ex, traceName ?? ex.Message);
+                    LogHelper.WriteLog(ex.Message, ex);
+                }
+            }
+            else
+            {
+                try
+                {
+                    action();
+                }
+                catch (Exception ex)
+                {
+                    if (enableThrow)
+                    {
+                        throw;
+                    }
+
+                    LogHelper.WriteLog(ex.Message, ex);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 通过异步方式调用的方式处理一些不太重要的工作。
+        /// </summary>
+        /// <param name="action">指定要执行的操作。</param>
+        /// <param name="traceName"></param>
+        public static void InvokeAsync(Action action, string traceName = null)
+        {
+            if (performanceTracer >= 0)
+            {
+                if (string.IsNullOrWhiteSpace(traceName))
+                {
+                    var method = new StackTrace().GetFrames()[1].GetMethod();
+
+                    traceName = string.Format("{0}.{1}", method.ReflectedType.FullName, method.Name);
+                }
+
+                Stopwatch stopwatch = new Stopwatch();
+
+                stopwatch.Start();
+
+                action.BeginInvoke(delegate
+                {
+                    stopwatch.Stop();
+
+                    if (stopwatch.Elapsed.TotalMilliseconds > performanceTracer)
+                    {
+                        if (!Debugger.IsAttached)
+                        {
+                            LogHelper.WriteLog("性能问题(" + traceName + ")" + stopwatch.Elapsed.TotalMilliseconds + "ms");
+                        }
+
+                        Debug.WriteLine("性能问题({0})，耗时{1}毫秒。", traceName, stopwatch.Elapsed.TotalMilliseconds);
+                    }
+                }, null);
+            }
+            else
+            {
+                action.BeginInvoke(null, null);
+            }
+        }
+        /// <summary>
+        /// 通过异步方式调用的方式处理一些不太重要的工作
+        /// </summary>
+        /// <param name="action"></param>
+        /// <param name="callback"></param>
+        /// <param name="traceName"></param>
+        public static void InvokeAsync(Action action, Action callback, string traceName = null)
+        {
+            if (performanceTracer >= 0)
+            {
+                if (string.IsNullOrWhiteSpace(traceName))
+                {
+                    var method = new StackTrace().GetFrames()[1].GetMethod();
+
+                    traceName = string.Format("{0}.{1}", method.ReflectedType.FullName, method.Name);
+                }
+
+                Stopwatch stopwatch = new Stopwatch();
+
+                stopwatch.Start();
+
+                action.BeginInvoke(delegate
+                {
+                    callback();
+                    stopwatch.Stop();
+
+                    if (stopwatch.Elapsed.TotalMilliseconds > performanceTracer)
+                    {
+                        if (!Debugger.IsAttached)
+                        {
+                            LogHelper.WriteLog("性能问题(" + traceName + ")" + stopwatch.Elapsed.TotalMilliseconds + "ms");
+                        }
+
+                        Debug.WriteLine("性能问题({0})，耗时{1}毫秒。", traceName, stopwatch.Elapsed.TotalMilliseconds);
+
+                    }
+                }
+                    , null);
+            }
+            else
+            {
+                action.BeginInvoke(null, null);
+            }
+        }
+    }
+}
---
+++ Weiz.TaskManager.TaskUtility/Utils/JobBase.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quartz;
+using Weiz.TaskManager.Common;
+using Weiz.TaskManager.Models;
+
+namespace Weiz.TaskManager.TaskUtility
+{
+    public class JobBase
+    {
+        /// <summary>
+        /// 执行指定任务
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="action"></param>
+        public void ExecuteJob(IJobExecutionContext context, Action action)
+        {
+            try
+            {
+                // 1. 获取Task 基本信息及 Task 配置的其他参数,任务启动时会读取配置文件节点的值传递过来
+                TaskModel task = QuartzHelper.GetTaskDetail(context);
+                // 2. 记录Task 运行状态数据库
+                DbLogHelper.WriteRunInfo(task.TaskName + " 开始", task.TaskID.ToString(), "");
+
+                // 3. 开始执行相关任务
+                PerformanceTracer.Invoke(action, task.TaskName);//监控并执行任务
+
+                // 4. 记录Task 运行状态数据库
+                DbLogHelper.WriteRunInfo(task.TaskName + " 结束", task.TaskID.ToString(), "成功执行");
+            }
+            catch (Exception ex)
+            {
+                JobExecutionException e2 = new JobExecutionException(ex);
+                //true  是立即重新执行任务 
+                e2.RefireImmediately = true;
+
+                // 记录异常到数据库和 log 文件中。
+                DbLogHelper.WriteErrorInfo(ex);
+
+            }
+        }
+    }
+}
